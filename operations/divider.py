5c226704805af93c410e45b7111b1be9d58f5bc1
Aulyaaaaa <aulll3446@gmail.com>
2025-09-08 12:05:15 +0700

    revert: batalkan penambahan fungsi divide_matrices (belum aman untuk pembagi nol)
    This reverts commit dd876b5688f7c9fa2daeb0a4ad7a2c9159c32214.

--- operations/divider.py
+++
@@ -1,9 +0,0 @@
-def divide_matrices(matrix1, matrix2):
-    result = []
-    for i in range(len(matrix1)):
-	row = []
-	for j in range(len(matrix1[0])):
-	    row.append(matrix1[i][j] / matrix2[i][j])
-
-	result.append(row)
-     return result
