496b934f841036aafdeebf2e12c462c0a5455035
Aulya Berlyana Agustin <aulll3446@gmail.com>
2025-10-13 14:40:53 +0700

    Update is_symmetric.py

--- validators/is_symmetric.py
+++ validators/is_symmetric.py
@@ -1,11 +1,27 @@
+# validators/is_symmetric.py
+"""
+Fungsi untuk memeriksa apakah sebuah matriks simetris.
+Matriks dikatakan simetris jika elemen [i][j] == [j][i].
+"""
+
 from validators.is_square import is_square
+
 def is_symmetric(matrix):
-    jumlah_baris = len(matrix)
-    jumlah_kolom = len(matrix[0])
-    if jumlah_baris != jumlah_kolom:
-	return False
-    for i in range(jumlah_baris):
-	for j in range(jumlah_kolom):
-	    if matrix[i][j] != matrix[j][i]:
-		return False
+    """
+    Mengecek apakah matriks simetris.
+    
+    Parameter:
+        matrix (list[list[float]]): matriks 2D
+        
+    Return:
+        bool: True jika simetris, False jika tidak
+    """
+    if not is_square(matrix):
+        return False
+
+    n = len(matrix)
+    for i in range(n):
+        for j in range(n):
+            if matrix[i][j] != matrix[j][i]:
+                return False
     return True
