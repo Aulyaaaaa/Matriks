827adcd7b843b2f56141006ce86ee72dcf0fb634
Aulya Berlyana Agustin <aulll3446@gmail.com>
2025-10-13 14:59:48 +0700

    Update _init_.py

--- operations/_init_.py
+++ operations/_init_.py
@@ -1,2 +1,20 @@
 # operations/__init__.py
-# subpackage for operation modules
+"""
+Package operations untuk proyek Matriks.
+
+Berisi fungsi-fungsi operasi pada matriks, seperti:
+- Transpose matriks
+- Invers matriks
+- Regresi linear (prediksi harga emas)
+"""
+
+from .transpose import transpose_matrix
+from .inverse import inverse_matrix
+from .regression import linear_regression, predict_price  # jika kamu punya file regression.py
+
+__all__ = [
+    "transpose_matrix",
+    "inverse_matrix",
+    "linear_regression",
+    "predict_price"
+]
