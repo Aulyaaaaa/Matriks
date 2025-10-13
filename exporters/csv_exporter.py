9e1b781c2bb10de738c80d51a29914f5090c1bc2
Aulyaaaaa <aulll3446@gmail.com>
2025-10-13 11:03:50 +0700

    UPdate exporters structure, remove old csv_exporter.py

---
+++ exporters/csv_exporter.py
@@ -0,0 +1,5 @@
+# Fungsi: export_to_csv(matriks, nama_file)
+# 1. Buka file dengan nama 'nama_file' dalam mode tulis.
+# 2. Buat objek 'writer' CSV.
+# 3. Tuliskan setiap baris data dari 'matriks' ke dalam file
+# 4. Tampilkan pesan sukses
---
+++ exporters/json_exporter.py
@@ -0,0 +1,6 @@
+# Fungsi: export_to_json(matriks, nama_file)
+# 1. Ubah data 'matriks' menjadi format yang dapat dipahami JSON
+# 2. Konversi data tersebut ke string JSON
+# 3. Buka file dengan nama 'nama_file' dalam mode tulis.
+# 4. Tuliskan data JSON ke dalam file
+# 5. Tampilkan pesan sukses 
